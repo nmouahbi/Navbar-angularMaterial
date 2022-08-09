c66753d45250f7afbcabce2a9f80b456b7afbf78
nmouahbi <83113523+nmouahbi@users.noreply.github.com>
2022-08-09 11:41:06 -0400

    updates

--- src/app/menu/offers/offers.component.ts
+++ src/app/menu/offers/offers.component.ts
@@ -21,6 +21,11 @@ export class OffersComponent implements OnInit {
     {name: 'Product 2', price: 200, description: 'This is a description', image: 'assets/image1.jpg' , altetxt: 'image2'  , button: 'Buy'},
     {name: 'Product 3', price: 300, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image3' , button: 'Buy'},
     {name: 'Product 4', price: 400, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image4'  , button: 'Buy'},
+    {name: 'Product 5', price: 500, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image5'  , button: 'Buy'},
+    {name: 'Product 6', price: 600, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image6'  , button: 'Buy'},
+    {name: 'Product 7', price: 700, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image7'  , button: 'Buy'},
+    {name: 'Product 8', price: 800, description: 'This is a description', image: 'assets/image1.jpg'   , altetxt: 'image8'  , button: 'Buy'},
+    
   ]
   constructor() { }
 
